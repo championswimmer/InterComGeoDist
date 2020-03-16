38af1a1832a688b31a0092a7124f7cd51d4859fe
Arnav Gupta <arnav@codingblocks.com>
2020-03-16 21:45:34 +0530

    save the output and write a basic readme
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Geo Distance Invitation List
+
+## Requirements
+
+1. Java (JDK 8 or above)
+2. Kotlin (not needed to install separately)
+
+## Compile and Run
+
+Compile project
+
+```shell
+./gradlew jar
+```
+
+Run Program
+
+```shell
+java -jar build/libs/IntercomGeoDist-1.0-SNAPSHOT.jar
+```
+
+## Run Tests
+
+```shell
+./gradlew check 
+```
--- src/main/kotlin/tech/arnav/intercomgeodist/Main.kt
+++ src/main/kotlin/tech/arnav/intercomgeodist/Main.kt
@@ -9,16 +9,20 @@ import tech.arnav.intercomgeodist.geo.GeoPoint
 
 fun main() {
     val dublin = GeoPoint(53.339428, -6.257664)
-    val nearDublin = CustomerFilter.Factory.distanceFilter(
-        dublin, 100.0
-    )
-    val userIdComparator = CustomerComparator.Factory.sortByProperty(
-        { it.userID }
-    )
-    print(
-        DataSource().getCustomers()
-            .filter(nearDublin)
-            .sortedWith(userIdComparator)
-            .toList().joinToString("\n")
-    )
+    val nearDublin = CustomerFilter.Factory.distanceFilter(dublin, 100.0)
+    val userIdComparator = CustomerComparator.Factory.sortByProperty({ it.userID })
+
+    println(" Users to invite ")
+    println("=================")
+    println("id \t | name")
+    println("-----------------")
+
+    DataSource().getCustomers()
+        .filter(nearDublin)
+        .sortedWith(userIdComparator)
+        .forEach {
+            println(
+                "${it.userID} \t | ${it.name}"
+            )
+        }
 }
